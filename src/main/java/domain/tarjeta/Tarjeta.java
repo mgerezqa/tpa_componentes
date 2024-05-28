721efef123aac11bab3e2a38e7fa238ab31b055a
NahuelLucana <nlucanadelgadillo@frba.utn.edu.ar>
2024-05-28 20:06:19 -0300

    Agrego private a atributos de la tarjeta, y que cantidadDisponiblePorDefecto sea static

--- src/main/java/domain/tarjeta/Tarjeta.java
+++ src/main/java/domain/tarjeta/Tarjeta.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Setter
 @Getter
 public class Tarjeta {
-    String codigoIdentificador; // Código alfanumérico de 11 caracteres
-    PersonaVulnerable titular;
-    private int cantidadDisponiblePorDefecto = 4;
+    private String codigoIdentificador; // Código alfanumérico de 11 caracteres
+    private PersonaVulnerable titular;
+    private static int cantidadDisponiblePorDefecto = 4;
     private int cantidadUsadaEnElDia;
     private List<RegistroDeUso> registros; //Debe quedar registrado, cuándo la usó, y en cuál heladera.
     //Constructor
@@ -25,7 +25,7 @@ public class Tarjeta {
         return 2*this.getTitular().cantidadDeMenoresACargo();
     }
     public int cantidadLimiteDisponiblePorDia(){
-        return this.getCantidadDisponiblePorDefecto() + this.cantidadDisponiblePorMenores();
+        return cantidadDisponiblePorDefecto + this.cantidadDisponiblePorMenores();
     }
     public boolean tieneCantidadDisponible(){
            return this.getCantidadUsadaEnElDia() < this.cantidadLimiteDisponiblePorDia();
