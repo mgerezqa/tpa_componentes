a3357e388d05c727e4c2cc112a8a7e36d3f277f5
NahuelLucana <nlucanadelgadillo@frba.utn.edu.ar>
2024-09-04 21:29:58 -0300

    Agrego faltante de persistencia del RegistroDeUso de la Tarjeta, y aun falta persistir la heladera que tiene el cada RegistroDeUso

--- src/main/java/domain/tarjeta/RegistroDeUso.java
+++ src/main/java/domain/tarjeta/RegistroDeUso.java
@@ -4,12 +4,20 @@ import domain.heladera.Heladera.Heladera;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@Entity
+@Table(name = "registro_uso_tarjeta")
 public class RegistroDeUso { //Cuándo la usó, y en cuál heladera.
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+    @Column(name = "registro_fecha_uso")
     private LocalDateTime datetime;
+    @Column(name = "heladera")
     private Heladera heladera;
 
     public RegistroDeUso(Heladera heladera){
