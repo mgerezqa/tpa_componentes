711ed75a6db0e640bd71f89d4a9765440857be0e
vgriggio <vgriggio@frba.utn.edu.ar>
2024-09-10 19:48:53 -0300

    Persistencia mejorada (faltan algunas cositas todavía)

--- src/main/java/domain/usuarios/ColaboradorFisico.java
+++ src/main/java/domain/usuarios/ColaboradorFisico.java
@@ -3,7 +3,9 @@ package domain.usuarios;
 import domain.donaciones.Vianda;
 import domain.geografia.area.AreaDeCobertura;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
