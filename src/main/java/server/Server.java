2385649da8a991dfe2cae2d879e214691bc41e1d
Mar Gerez <mingerez@gmail.com>
2024-12-07 17:26:11 -0300

    Corrijo Clase Server en master para que motor de plantillas encuentre a index.hbs.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -76,7 +76,7 @@ public class Server {
                 try {
                     String templatePath = path.replace(".hbs", "");
                     // La plantilla debe cargarse desde la carpeta "templates" en el classpath
-                    template = handlebars.compile("templates/" + templatePath);
+                    template = handlebars.compile("templates" + templatePath);
                     return template.apply(model);
                 } catch (IOException e) {
                     e.printStackTrace();
