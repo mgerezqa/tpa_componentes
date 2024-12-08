ef6373fffef08e51b7390ebe1fe780ae0645be5d
Mar Gerez <mingerez@gmail.com>
2024-12-08 20:06:55 -0300

    Aplico cambios en Clase PeoresContrasenias.

--- src/main/java/utils/validadorDeContrasenias/politicas/PeoresContrasenias.java
+++ src/main/java/utils/validadorDeContrasenias/politicas/PeoresContrasenias.java
@@ -2,11 +2,9 @@ package utils.validadorDeContrasenias.politicas;
 
 import domain.Config;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 
-public class PeoresContrasenias implements IPoliticas{
+public class PeoresContrasenias implements IPoliticas {
     @Override
     public void chequearPolitica(String nombreUsuario, String contrasenia) throws Exception {
         String rutaArchivo = Config.getInstance().getProperty("peores_contrasenias_ruta");
@@ -14,21 +12,20 @@ public class PeoresContrasenias implements IPoliticas{
             throw new Exception("No se configuró la ruta del archivo en config.properties");
         }
 
-        // Leer el archivo
-        File file = new File(rutaArchivo);
-        if (!file.exists()) {
-            throw new Exception("El archivo especificado no existe: " + rutaArchivo);
-        }
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(rutaArchivo)) {
+            if (inputStream == null) {
+                throw new Exception("No se pudo encontrar el archivo: " + rutaArchivo);
+            }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = reader.readLine()) != null) {
                 if (line.equals(contrasenia)) {
                     throw new Exception("La contraseña no es válida, pertenece a una de las peores 10.000 contraseñas");
                 }
             }
+        } catch (IOException e) {
+            throw new Exception("Error al leer el archivo de contraseñas: " + e.getMessage());
         }
-
-        return;
     }
 }
